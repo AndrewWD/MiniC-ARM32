7710c854c0fcd1b90c798cca67a340a652307236
Yang Gao <yg1751@nyu.edu>
2019-02-20 22:33:20 -0500

    Implement part of String's definition

--- Compiler.hx
+++ Compiler.hx
@@ -14,30 +14,14 @@ module org.crsx.hacs.samples.Compiler {
     token FUNCTION  | function ;
 
     token Integer      | ⟨Digit⟩+ ;          // tokens  
-    token Identifier   | ⟨IDChar⟩ (⟨IDChar⟩ | ⟨Digit⟩)+ ;
-    
-    token fragment Digit  | [0-9] ;    // token fragments
-    token fragment IDChar | ([a-zA-Z$_]) ;
+    // token Identifier   | ⟨IDChar⟩ (⟨IDChar⟩ | ⟨Digit⟩)+ ;
+    token String       | \" (⟨StrChar⟩ | ⟨Escapes⟩)+ \" ;
 
-    /* Temporary code for testing lexical part
+    token fragment Digit    | [0-9] ;    // token fragments
+    token fragment IDChar   | [a-zA-Z$_] ;
+    token fragment StrChar  | [^\"\\\n\r] ;  
+    token fragment NewLine  | \n | \r\n | \r ;
+    token fragment Escapes  | \\⟨NewLine⟩ | \\\" | \\\\ | \\n | \\t ;
 
-    // token INTE    | ⟨Digit⟩+ ;                       // tokens
-    // token FLOAT  | ⟨INTE⟩ "." ⟨INTE⟩ ;
-    // token ID     | ⟨Lower⟩+ ('_'? ⟨INTE⟩)? ;
-
-    // token fragment Lower  | [a-z] ;
-    // /* SYNTAX ANALYSIS. */
-
-    // sort Exp   | ⟦ ⟨Exp@1⟩ + ⟨Exp@2⟩ ⟧@1            // addition
-    //         | ⟦ ⟨Exp@2⟩ * ⟨Exp@3⟩ ⟧@2            // multiplication
-    //         | ⟦ ⟨INTE⟩ ⟧@3                        // integer
-    //         | ⟦ ⟨FLOAT⟩ ⟧@3                      // floating point number
-    //         | ⟦ ⟨Name⟩ ⟧@3                       // assigned value
-    //         | sugar ⟦ (⟨Exp#⟩) ⟧@3 → Exp# ;       // parenthesis
-
-    // sort Name  | symbol ⟦ ⟨ID⟩ ⟧ ;                  // assigned symbols
-
-    // main sort Stat  | ⟦ ⟨Name⟩ := ⟨Exp⟩ ; ⟨Stat⟩ ⟧
-    //                 | ⟦ { ⟨Stat⟩ } ⟨Stat⟩ ⟧
-    //                 | ⟦⟧ ;
+    main sort Stat | ⟦⟧ ;
 }
